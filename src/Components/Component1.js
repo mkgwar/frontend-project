24e1897e3a942bbdf180148803c41db301ef521e
mkgwar <mohitgangwarknp@gmail.com>
2021-12-30 19:40:14 +0530

    className fix

--- src/Components/Component1.js
+++ src/Components/Component1.js
@@ -2,7 +2,7 @@ import "./Component1.scss";
 
 const Component1 = ({ title, paragraph, flex }) => {
   return (
-    <div className={"component1" + " " + flex}>
+    <div className={"component1 " + flex}>
       <div className="component1__text-content">
         <h2 className="component1__title">{title}</h2>
         <p className="component1__paragraph">{paragraph}</p>
