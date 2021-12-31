8aabc25a8d480b7f353371087a575f8904e3da4f
mkgwar <mohitgangwarknp@gmail.com>
2021-12-31 11:32:27 +0530

    removed moving image footer

--- src/Components/ImageFooter.js
+++ src/Components/ImageFooter.js
@@ -7,18 +7,10 @@ import img4 from "../images/desktop/image-gallery-sugarcubes.jpg";
 const ImageFooter = () => {
   return (
     <div className="images">
-      <div className="xyz1">
-        <img src={img1} alt="footer" />
-        <img src={img2} alt="footer" />
-        <img src={img3} alt="footer" />
-        <img src={img4} alt="footer" />
-      </div>
-      <div className="xyz2">
-        <img src={img1} alt="footer" />
-        <img src={img2} alt="footer" />
-        <img src={img3} alt="footer" />
-        <img src={img4} alt="footer" />
-      </div>
+      <img src={img1} alt="footer" />
+      <img src={img2} alt="footer" />
+      <img src={img3} alt="footer" />
+      <img src={img4} alt="footer" />
     </div>
   );
 };
